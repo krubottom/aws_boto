05a484109af386ac55e71aca40638642c2f4f198
krubottom <karl.rubottom@gmail.com>
2017-10-22 21:23:52 -0600

    Formatting

--- r53.py
+++ r53.py
@@ -5,5 +5,11 @@ r53client = boto3.client('route53')
 
 r53res = r53client.list_hosted_zones_by_name()
 
+print "\n"
+
 for zone in r53res['HostedZones']:
     print zone['Name'][:-1] + " - " + zone['Id']
+    for record in r53client.list_resource_record_sets(HostedZoneId=zone['Id'])['ResourceRecordSets']:
+        if record['Name'] != zone['Name']:
+            print record['Name'][:-1]
+    print "-----\n"
