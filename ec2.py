46fd551c47fd741647ee3882d8e3c3432101702a
krubottom <karl.rubottom@gmail.com>
2017-10-22 21:57:27 -0600

    Name and IP

--- ec2.py
+++ ec2.py
@@ -3,11 +3,18 @@ import boto3
 
 ec2client = boto3.client('ec2')
 
-#print ec2client.describe_instances()
-
 ec2res = ec2client.describe_instances()
 
+def get_instance_name(ec2id):
+    ec2 = boto3.resource('ec2')
+    ec2instance = ec2.Instance(ec2id)
+    instancename = ''
+    for tags in ec2instance.tags:
+        if tags["Key"] == 'Name':
+            instancename = tags["Value"]
+    return instancename
+
+
 for res in ec2res['Reservations']:
-    for host in res['Instances']:
-        print host['Tags']
-        print host['PublicIpAddress']
+    for instance in res['Instances']:
+        print get_instance_name(instance['InstanceId']) + " - " + instance['PublicIpAddress']
